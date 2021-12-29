3370190e9c06553ce17b3c67f4d60f0489e1e1ba
JuanGPC <juangpc@users.noreply.github.com>
2021-12-29 16:49:03 -0500

    add main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
+#define BAR(x) ((x) || (x)) 
 
+template <typename T>
+void foo(T x) {
+  for (unsigned i = 0; i < 10; ++i) {
+    BAR(i);
+  }
+}
 
 
 int main(int argc, char* argv[]) {
-
+  foo<int>(0);
+  foo<float>(0);
   return 0;
 }
 
